b67c7871524ab95e93851932ef0724fdc005a3c9
harshitagarwal04 <harshitagarwal713@gmail.com>
2025-03-22 20:42:02 +0530

    TakeTwo

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import CameraScreen from './screens/CameraScreen';
 import GalleryScreen from './screens/GalleryScreen';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins: require('@expo-google-fonts/poppins/Poppins_400Regular.ttf'),
+    Roboto: require('@expo-google-fonts/roboto/Roboto_400Regular.ttf'),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+      if (fontsLoaded) {
+        await SplashScreen.hideAsync();
+      }
+    }
+    prepare();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="LeafLens">
+        <Stack.Screen name="LeafLens" component={HomeScreen} />
         <Stack.Screen name="Camera" component={CameraScreen} />
         <Stack.Screen name="Gallery" component={GalleryScreen} />
       </Stack.Navigator>
