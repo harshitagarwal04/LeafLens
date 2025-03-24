850b3dd094e212191b3d1d9d2fc636624d7781ba
harshitagarwal04 <harshitagarwal713@gmail.com>
2025-03-24 18:45:04 +0530

    New Code Optimization

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import CameraScreen from './screens/CameraScreen';
 import GalleryScreen from './screens/GalleryScreen';
+import AnalyzeScreen from './screens/AnalyzeScreen';
 import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
+import Toast from 'react-native-toast-message';
 
 const Stack = createStackNavigator();
 
@@ -30,13 +32,17 @@ const App = () => {
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="LeafLens">
-        <Stack.Screen name="LeafLens" component={HomeScreen} />
-        <Stack.Screen name="Camera" component={CameraScreen} />
-        <Stack.Screen name="Gallery" component={GalleryScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="LeafLens">
+          <Stack.Screen name="LeafLens" component={HomeScreen} />
+          <Stack.Screen name="Camera" component={CameraScreen} />
+          <Stack.Screen name="Gallery" component={GalleryScreen} />
+          <Stack.Screen name="AnalyzeScreen" component={AnalyzeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <Toast />
+    </>
   );
 };
 
